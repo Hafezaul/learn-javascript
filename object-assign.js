1e74d38b64e69a16c053dbf6de6b2cca0369ff6e
faris.al.afiz <faris.al.afiz@gmail.com>
2025-06-05 07:20:37 +0000

    feat : update object.assign

--- object-assign.js
+++ object-assign.js
@@ -11,10 +11,26 @@ let copyData = Object.assign({}, data1[0], data2[0]);
 console.log(copyData)
 
 
-// percobaan tanpa bracket []
+// percobaan tanpa bracket {}
 
 const user = { name : "Faris Hafidz"}; 
 const extra = { age : 7, hobby : "Ngoding"}; 
 
 let copyUser = Object.assign(user, extra);
 console.log(copyUser);
+
+
+// 3 penyalinan object dengan object.assign()
+
+const apps = { 
+    nama : "Ahmad Kashmiri Hendra",
+    alamat : "Jln. Mangga 5"
+}; 
+const noTelp = { 
+    no_telp : '086548485754',
+};
+const mobil = { 
+    merek : "Avanza",
+};
+const copyApss = Object.assign(apps, noTelp, mobil);
+console.log(copyApss);
